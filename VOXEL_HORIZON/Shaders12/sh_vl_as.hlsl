f48b7b8bb416203568dacefc000e23e29b9cc813
megayuchi <megayuchi@gmail.com>
2023-10-31 16:32:00 +0900

    client_x64_release.exe fixed

--- VOXEL_HORIZON/Shaders12/sh_vl_as.hlsl
+++ VOXEL_HORIZON/Shaders12/sh_vl_as.hlsl
@@ -13,55 +13,6 @@ groupshared Payload s_Payload;
 void asDefault(uint gtid : SV_GroupThreadID, uint DispatchID : SV_DispatchThreadID, uint groupID : SV_GroupID)
 {
     bool visible = false;
- 
-    bool bUseIntstancing = false;
-    uint MaxInstanceCount = g_MeshShaderInstanceCount;
-    if (MaxInstanceCount > 0)
-    {
-        bUseIntstancing = true;
-    }
-    else
-    {
-        MaxInstanceCount = 1;
-    }
-    //
-    //CONSTANT_BUFFER_SHADOW_CASTER구조체와CONSTANT_BUFFER_DEFAULT구조체중 어느쪽을 억세스 하는가?
-    //
-    uint InstanceObjID = DispatchID / MeshletNumPerFaceGroup;
-    uint meshltID = DispatchID % MeshletNumPerFaceGroup;
-    
-    if (InstanceObjID < MaxInstanceCount && meshltID < MeshletNumPerFaceGroup)
-    {
-        MESHLET meshlet = g_MeshletBuffer[meshltID];
-        float4 SpherePosWorld = mul(float4(meshlet.Bounds.xyz, 1), matWorld); // 월드공간에서의 스피어 중점
-        if (bUseIntstancing)
-        {
-            SpherePosWorld = mul(float4(SpherePosWorld.xyz, 1), g_InstanceMatWorldList[InstanceObjID]); // 월드공간에서의 버텍스 좌표
-        }
-#ifdef USE_MESHLET_CULLING
-        visible = CullSphere(g_FrustumPlanes, SpherePosWorld.xyz, meshlet.Bounds.w) != 0;
-#else
-        visible = true;
-#endif
-    }
-    // Compact visible meshlets into the export payload array
-    if (visible)
-    {
-    // 현재 스레드가 테스트한 meshlet이 보여진다고 판단된 경우, s_Payload배열에 중복없이 원소의 빈 공간 없이 붙여서(packed) 기록
-        uint index = WavePrefixCountBits(visible);
-        s_Payload.MeshletID[index] = meshltID; // write meshlet index to array(packed)
-        s_Payload.InstanceID[index] = InstanceObjID; // write InstanceID index to array(packed)
-    }
-     // Dispatch the required number of MS threadgroups to render the visible meshlets
-    uint visibleCount = WaveActiveCountBits(visible);
-    DispatchMesh(visibleCount, 1, 1, s_Payload);
-}
-/*
-
-[NumThreads(AS_GROUP_SIZE, 1, 1)]
-void asDefault(uint gtid : SV_GroupThreadID, uint DispatchID : SV_DispatchThreadID, uint groupID : SV_GroupID)
-{
-    bool visible = false;
     
     // instancing사용할 경우, DispatchID = MeshletNumPerFaceGroup * Instance Cout - 1;
     uint InstanceObjID = DispatchID / MeshletNumPerFaceGroup;
@@ -104,4 +55,3 @@ void asDefault(uint gtid : SV_GroupThreadID, uint DispatchID : SV_DispatchThread
     uint visibleCount = WaveActiveCountBits(visible);
     DispatchMesh(visibleCount, 1, 1, s_Payload);
 }
-*/
